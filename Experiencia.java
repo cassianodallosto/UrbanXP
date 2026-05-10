e62d9d0940fef53d1122e8d6f9abf12438613b91
DaviNachtigall <nachtigalldavi@gmail.com>
2026-05-09 23:58:14 -0300

    Criação das classes Experiencia, Passeios, Show e Workshop.

--- Experiencia.java
+++ Experiencia.java
@@ -1,10 +1,37 @@
 package model;
-
+import java.time.LocalDateTime;
 public abstract class Experiencia {
     private String titulo;
     private String descricao;
-    private String dataHora;
+    private LocalDateTime dataHora;
     private int capacidadeMax;
     private double precoBase;
 
+    public Experiencia(String titulo, String descricao, LocalDateTime dataHora, int capacidadeMax, double precoBase){
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataHora = dataHora;
+        this.capacidadeMax = capacidadeMax;
+        this.precoBase = precoBase;
+        }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public int getCapacidadeMax() {
+        return capacidadeMax;
+    }
+
+    public double getPrecoBase() {
+        return precoBase;
+    }
 }
--- Passeios.java
+++ Passeios.java
@@ -1,8 +1,32 @@
 package model;
 
-public class Passeios extends Experiencia{]
+import java.time.LocalDateTime;
+
+public class Passeios extends Experiencia{
     private String pontoEncontro;
     private String guiaResponsavel;
 
+    public Passeios(String titulo, String descricao, LocalDateTime dataHora, int capacidadeMax, double precoBase,
+                            String pontoEncontro, String guiaResponsavel) {
+        super(titulo, descricao, dataHora, capacidadeMax, precoBase);
+        this.pontoEncontro = pontoEncontro;
+        this.guiaResponsavel = guiaResponsavel;
+    }
 
+    public String getPontoEncontro() {
+        return pontoEncontro;
+    }
+
+    public String getGuiaResponsavel() {
+        return guiaResponsavel;
+    }
+
+
+    public String gerarResumo() {
+        return "PASSEIO TURÍSTICO: " + getTitulo() +
+                "\nGuia: " + guiaResponsavel +
+                "\nPonto de encontro: " + pontoEncontro +
+                "\nData: " + getDataHora() +
+                "\nPreço base: R$" + getPrecoBase();
+    }
 }
