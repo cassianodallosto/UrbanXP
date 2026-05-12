3576ff0a10fece253d1b75953494de1c06e89f24
mariaefs <mariaefs.aluno@unipampa.edu.br>
2026-05-12 15:58:47 -0300

    Update Ingresso.java
    implementação da classe ingresso

--- Ingresso.java
+++ Ingresso.java
@@ -1,5 +1,28 @@
 package model;
 
 public class Ingresso {
+    private Cliente cliente;
+    private Experiencia experiencia;
+    private double precofinal;
+    private TicketSituation situacao;
 
+    public Ingresso(Cliente cliente, Experiencia experiencia) {
+        this.cliente = cliente;
+        this.experiencia = experiencia;
+        this.precofinal = cliente.getTipoCliente().calcularPreco(experiencia.getPrecoBase());
+        this.situacao = TicketSituation.RESERVADO;
+    }
+    public void pagar(){
+        situacao = TicketSituation.PAGO;
+    }
+    public void cancelar(){
+        situacao = TicketSituation.CANCELADO;
+    }
+    public void exebirIngresso(){
+        System.out.println("Cliente: " + cliente.getNome());
+        System.out.println("Experiência: " + experiencia.getTitulo());
+        System.out.println("Preço final: R$" + precofinal);
+        System.out.println("Situação: " + situacao.getDescricao());
+    }
+    
 }
